7be9ea62d8265f296a0e21d1f9987ba6992bf98a
me <20161564@vision.hoseo.edu>
2021-06-03 13:51:31 +0900

    스타일 변경 & API 적용 및 진료상세 컴포넌트 구현

--- components/table.js
+++ components/table.js
@@ -22,7 +22,6 @@ class Table {
         }); view.forEach(item => this.table.appendChild(item))
     }
     display(flag) { if(this.table) { flag ? this.table.style.display = 'table' : this.table.style.display = 'none' } }
-    setTitle(title) { this.title = title }
     getStructor() {
         if(this.columns === this.hist) {
             this.hist = this.columns
@@ -31,19 +30,16 @@ class Table {
 
         const   table = document.createElement('table'),
                 thead = document.createElement('thead'),
-                title = document.createElement('div'),
                 tr = document.createElement('tr')
 
         this.columns.forEach(col_name => {
             const th = document.createElement('th'); th.innerHTML = col_name
             tr.appendChild(th)
         })
-        title.innerHTML = this.title
-        title.className = 'title'
         thead.appendChild(tr); table.appendChild(thead);
-        table.id = 'uniq_table'; this.target.innerHTML = ''
-        this.target.appendChild(title)
-        this.target.appendChild(table)
+        table.id = 'uniq_table';this.target.innerHTML = ''
+        
+        this.target.appendChild(table); 
         this.table = table
     }
 }
--- components/treatment.js
+++ components/treatment.js
@@ -1,32 +1,75 @@
+class Video {
+    constructor({target, filename, onError}) {
+        this.target = target
+        this.filename = filename
+        this.onError = onError
+        this.render()
+    }
+    render() {
+        const source = document.createElement('source')
+        Api(`/treatment/info/${this.filename}`).then( res => {
+            if(res.status) {
+                console.log()
+                this.onError(`${res.status} ERROR`)
+            } else {
+                setAttributes(source, { src : res, type : 'video/mp4' })
+                this.target.appendChild(source)
+            }
+        })
+    }
+}
+
 class Treatment {
-    constructor({target}) {
+    constructor({target, error}) {
+        this.resultBox = document.createElement('div')
         this.target = document.getElementById(target)
+        this.resultBox.className = 'result-box'
+        this.error = error
         this.display(false)
     }
-    getItemStructor({video, measure}) {
+    onFetchError(msg) {
+        if(msg) {
+            this.display(false)
+            this.error.setState(msg)
+            this.error.display(true)
+        } else this.error.display(true)
+    }
+    getItemStructor(video, measure) {
         const   result = document.createElement('div'),
-                video_container = document.createElement('div'),
                 treatment = document.createElement('div'),
-                video = document.createElement('video')
-
+                video_tag = document.createElement('video'),
+                video_container = document.createElement('div')
+        
         result.className = 'result'
-        video_container.className = 'video'
         treatment.className = 'treatment'
+        video_container.className = 'video'
+        new Video({
+            target : video_tag,
+            filename : video.fileName,
+            onError : msg => { this.onFetchError(msg) }
+        })
 
-        Object.keys(measure).map(key => {
-            const   item = document.createElement('div'),
-                    key = document.createElement('div'),
+        Object.keys(measure).forEach(_key => { 
+            const   key = document.createElement('div'),
+                    item = document.createElement('div'),
                     value = document.createElement('span')
-            key.innerHTML = key; value.innerHTML = measure[key]
+            item.className='item'
+            key.innerHTML = _key; value.innerHTML = measure[_key]
             item.appendChild(key); item.appendChild(value)
             treatment.appendChild(item)
         })
-        result.appendChild(video)
-        result.appendChild(treatment)
+
+        result.appendChild(video_tag); result.appendChild(treatment)
         return result
     }
     setState(results) {
-        
+        results.forEach(res => { 
+            const {file, ...args} = res
+            const child = this.getItemStructor(file, args)
+            this.resultBox.appendChild(child)
+        })
+        this.target.appendChild(this.resultBox)
+        this.display(true)
     }
-    display(flag) { flag ? this.target.style.display = 'grid' : this.target.style.display = 'none' }
+    display(flag) { flag ? this.resultBox.style.display = 'grid' : this.resultBox.style.display = 'none' }
 }
